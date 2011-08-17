cebe541d21b98d6d90677aa73291f573272c44b5
Andrew Dunkman <adunkman@Benny.local>
2011-08-16 21:54:31 -0500

    Revert "Reverted application to initial tutorial state. Step 1."
    This reverts commit 9fcdd777003fa6f469cf117721a98d96e79a0011.

--- script/viewModels/viewModel.list.js
+++ script/viewModels/viewModel.list.js
@@ -10,8 +10,130 @@
    
    var shot = window.courrrt.shot;
    
-   var list = {
-   };
+   var list = $.extend(ko.observable(), {
+      feeds: {
+         popular: "http://api.dribbble.com/shots/popular",
+         everyone: "http://api.dribbble.com/shots/everyone",
+         debuts: "http://api.dribbble.com/shots/debuts"
+      },
+
+      shots: ko.observableArray(),
+      stats: ko.observable(),
+      state: ko.observable()
+   });
+
+   list.reset = function () {
+      this.stats({
+         page: null,
+         pages: null,
+         per_page: null,
+         total: null
+      });
+      this.shots([]);
+   }.bind(list);
+   
+   list.reset();
+   
+   list.fetch = function () {
+      var page = this.stats().page;
+      page = page === null ? 1 : page + 1;
+      
+      if (this.stats().pages !== null && this.stats().pages < page) {
+         return;
+      }
+      
+      if (this.state() === "loading") {
+         return;
+      }
+      
+      this.state("loading");
+      
+      $.ajax({
+         url: this.feeds[this()],
+         method: "get",
+         data: { page: page, per_page: 30 },
+         dataType: "jsonp",
+         success: function (data) {
+            
+            var shots = this.shots();
+            $.each(data.shots, function (index, shotData) {
+               shots.push(new shot(shotData));
+            });
+            this.shots(shots);
+            
+            this.stats({
+               page: data.page,
+               pages: data.pages,
+               per_page: data.per_page,
+               total: data.total
+            });
+            
+            this.state("");
+            
+         }.bind(this)
+      });
+   }.bind(list);
+   
+   list.next = function () {
+      this.go(1);
+   }.bind(list);
+   
+   list.previous = function () {
+      this.go(-1);
+   }.bind(list);
+   
+   list.go = function (shift) {
+      var index, selected = this.shot(), shots = this.list.shots();
+      
+      if (selected === null) {
+         this.shot(shots[0]);
+         this.list.scrollTo(shots[0]);
+         return;
+      }
+      
+      // Find the index of the selected shot.
+      $.each (shots, function (i, shot) {
+         if (selected.id === shot.id) {
+            index = i;
+            return false; // break
+         }
+      });
+      
+      if (index === undefined) {
+         return;
+      }
+      
+      index += shift;
+      
+      if (index < 0) {
+         index = 0;
+      }
+      if (index >= shots.length) {
+         index = shots.length - 1;
+      }
+      
+      this.shot(shots[index]);
+      this.list.scrollTo(shots[index]);
+   }.bind(window.courrrt.viewModel);
+   
+   list.scrollTo = function (shot) {
+      var $element = $("img[src='" + shot.image.teaser_url + "']");
+      var scrollTop = $("html, body").scrollTop(), 
+         windowHeight = $(window).height(),
+         offset = $element.offset().top,
+         height = $element.height();
+      
+      if (offset + height > scrollTop + windowHeight || offset < scrollTop) {
+         $("html, body").stop().animate({
+            scrollTop: offset + height - windowHeight / 2
+         }, 500, "easeInOutExpo");
+      }
+   }.bind(list);
+
+   list.subscribe(function () {
+      this.reset();
+      this.fetch();
+   }, list);
    
    // Export to global namespace: courrrt.viewModel.list
    $.extend(window.courrrt.viewModel, { list: list });
--- script/viewModels/viewModel.shot.js
+++ script/viewModels/viewModel.shot.js
@@ -4,9 +4,36 @@
       throw "courrrt.viewModel is required."
    }
    
-   var shot = {
-   };
-      
+   var shot = ko.observable(null);
+   
+   shot.open = function () {
+     if (this() === null) {
+        return;
+     }
+     
+     // Open a new window with this shot in it.
+     window.open(this().url, "_blank"); 
+   }.bind(shot);
+   
+   
+   shot.openPlayer = function () {
+     if (this() === null) {
+        return;
+     }
+     
+     // Open a new window with this shot in it.
+     window.open(this().player.dribbble.profile, "_blank");
+   }.bind(shot);
+   
+   
+   shot.reset = function () {
+      this(null);
+   }.bind(shot);
+   
+   window.courrrt.viewModel.reset.subscribe(shot.reset);
+   window.courrrt.viewModel.list.subscribe(shot.reset);
+   
+   
    // Export to global namespace: courrrt.viewModel.list
    $.extend(window.courrrt.viewModel, { shot: shot });
    
