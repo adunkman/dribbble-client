be229c130b672da6f8a086ba4df0ee1ddad14718
Andrew Dunkman <adunkman@Benny.local>
2011-08-16 23:03:34 -0500

    Step 5

--- script/viewModels/viewModel.list.js
+++ script/viewModels/viewModel.list.js
@@ -18,30 +18,69 @@
       }, 
       
       shots: ko.observableArray(),
-      state: ko.observable()
+      state: ko.observable(),
+      stats: ko.observable()
    });
    
-   list.subscribe(function () {
+   list.reset = function () {
+      
+      this.stats({
+         page: null,
+         pages: null,
+         per_page: null,
+         total: null
+      });
+      
+      this.shots([]);
+      
+   }.bind(list);
+   
+   list.reset();
+   
+   list.fetch = function () {
+      
+      var page = this.stats().page;
+      page = page === null ? 1 : page + 1;
+      
+      if (this.stats().pages !== null && this.stats().pages < page) {
+         return;
+      }
+      
+      if (this.state() === "loading") {
+         return;
+      }
       
       this.state("loading");
       
       $.ajax({
          url: this.feeds[this()],
          method: "get",
-         data: { page: 1, per_page: 30 },
+         data: { page: page, per_page: 30 },
          dataType: "jsonp",
          success: function (data) {
             
-            var shots = [];
+            var shots = this.shots();
             $.each(data.shots, function (index, shotData) {
                shots.push(new shot(shotData));
             });
             this.shots(shots);
             this.state("");
             
+            this.stats({
+               page: data.page,
+               pages: data.pages,
+               per_page: data.per_page,
+               total: data.total
+            });
+            
          }.bind(this)
       });
       
+   }.bind(list);
+   
+   list.subscribe(function () {
+      this.reset();
+      this.fetch();
    }, list);
    
    // Export to global namespace: courrrt.viewModel.list
