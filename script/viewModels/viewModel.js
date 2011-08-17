6a7451b3daa12ed99df4fb5af75042e94c72649a
Andrew Dunkman <adunkman@Benny.local>
2011-08-16 23:18:37 -0500

    Step 6

--- script/viewModels/viewModel.js
+++ script/viewModels/viewModel.js
@@ -1,7 +1,17 @@
 (function ($, window) {
    
    var viewModel = {
+      reset: ko.observable()
    };
+   
+   viewModel.reset.subscribe(function () {
+      
+      // Scroll to top
+      $("html, body").stop().animate({
+         scrollTop: 0
+      }, 1000, "easeInOutExpo");
+      
+   });
    
    // Export to global namespace: courrrt.viewModel
    $.extend(window, { courrrt: window.courrrt || { } });
--- script/viewModels/viewModel.shot.js
+++ script/viewModels/viewModel.shot.js
@@ -5,6 +5,33 @@
    }
    
    var shot = ko.observable();
+   
+   shot.reset = function () {
+      
+      this(null);
+      
+   }.bind(shot);
+   
+   shot.open = function () {
+     if (this() === null) {
+        return;
+     }
+     
+     // Open a new window with this shot in it.
+     window.open(this().url, "_blank"); 
+   }.bind(shot);
+   
+   shot.openPlayer = function () {
+     if (this() === null) {
+        return;
+     }
+     
+     // Open a new window with this shot in it.
+     window.open(this().player.dribbble.profile, "_blank");
+   }.bind(shot);
+   
+   window.courrrt.viewModel.reset.subscribe(shot.reset);
+   window.courrrt.viewModel.list.subscribe(shot.reset);
       
    // Export to global namespace: courrrt.viewModel.list
    $.extend(window.courrrt.viewModel, { shot: shot });
