9fcdd777003fa6f469cf117721a98d96e79a0011
Andrew Dunkman <adunkman@gmail.com>
2011-08-16 20:59:22 -0500

    Reverted application to initial tutorial state. Step 1.

--- script/viewModels/viewModel.js
+++ script/viewModels/viewModel.js
@@ -1,19 +1,7 @@
 (function ($, window) {
    
    var viewModel = {
-      title: "courrrt",
-      
-      reset: ko.observable()
    };
-   
-   viewModel.reset.subscribe(function () {
-      
-      // Scroll to top
-      $("html, body").stop().animate({
-         scrollTop: 0
-      }, 1000, "easeInOutExpo");
-      
-   });
    
    // Export to global namespace: courrrt.viewModel
    $.extend(window, { courrrt: window.courrrt || { } });
--- script/viewModels/viewModel.shot.js
+++ script/viewModels/viewModel.shot.js
@@ -4,36 +4,9 @@
       throw "courrrt.viewModel is required."
    }
    
-   var shot = ko.observable(null);
-   
-   shot.open = function () {
-     if (this() === null) {
-        return;
-     }
-     
-     // Open a new window with this shot in it.
-     window.open(this().url, "_blank"); 
-   }.bind(shot);
-   
-   
-   shot.openPlayer = function () {
-     if (this() === null) {
-        return;
-     }
-     
-     // Open a new window with this shot in it.
-     window.open(this().player.dribbble.profile, "_blank");
-   }.bind(shot);
-   
-   
-   shot.reset = function () {
-      this(null);
-   }.bind(shot);
-   
-   window.courrrt.viewModel.reset.subscribe(shot.reset);
-   window.courrrt.viewModel.list.subscribe(shot.reset);
-   
-   
+   var shot = {
+   };
+      
    // Export to global namespace: courrrt.viewModel.list
    $.extend(window.courrrt.viewModel, { shot: shot });
    
