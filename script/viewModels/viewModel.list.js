c8d905d8af0d92021eade00b1690c505aae3b82e
Andrew Dunkman <adunkman@Benny.local>
2011-08-16 22:25:31 -0500

    Step 2

--- script/viewModels/viewModel.list.js
+++ script/viewModels/viewModel.list.js
@@ -10,8 +10,35 @@
    
    var shot = window.courrrt.shot;
    
-   var list = {
-   };
+   var list = $.extend(ko.observable(), {
+      feeds: {
+         popular: "http://api.dribbble.com/shots/popular",
+         everyone: "http://api.dribbble.com/shots/everyone",
+         debuts: "http://api.dribbble.com/shots/debuts"
+      }, 
+      
+      shots: ko.observableArray()
+   });
+   
+   list.subscribe(function () {
+      
+      $.ajax({
+         url: this.feeds[this()],
+         method: "get",
+         data: { page: 1, per_page: 30 },
+         dataType: "jsonp",
+         success: function (data) {
+            
+            var shots = [];
+            $.each(data.shots, function (index, shotData) {
+               shots.push(new shot(shotData));
+            });
+            this.shots(shots);
+            
+         }.bind(this)
+      });
+      
+   }, list);
    
    // Export to global namespace: courrrt.viewModel.list
    $.extend(window.courrrt.viewModel, { list: list });
